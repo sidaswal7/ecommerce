8c2011c2ee62a9f7231f4c793e7cd3c8782dded5
sidaswal7 <siddhantaswal7@gmail.com>
2023-12-20 21:24:25 +0530

    site created

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+export const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+        <div className="descriptionbox-navigator">
+            <div className="descriptionbox-nav-box">
+                Description
+            </div>
+            <div className="descriptionbox-nav-box fade">
+                Reviews(122)
+            </div>
+        </div>
+        <div className="descriptionbox-description">
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est tempore, eaque laborum doloribus necessitatibus consequatur commodi ad eius similique impedit tempora ullam magni facilis dignissimos beatae. Pariatur exercitationem consequatur Lorem ipsum dolor sit amet consectetur adipisicing elit. Id ducimus, perspiciatis molestiae architecto iusto incidunt minus reiciendis, maxime magni doloremque debitis. Explicabo ducimus eum nostrum iure molestiae amet ab praesentium.</p>
+            <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta eos omnis magni blanditiis debitis cupiditate tempora ipsam nemo aliquid esse mollitia sunt, nulla similique saepe, facere iusto non ullam autem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam quae temporibus.
+            </p>
+        </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
